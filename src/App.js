2df90f1a484d294d84bb70eb34249916e53131cd
Z Ding <dinzonnyuoe@gmail.com>
2022-11-12 19:49:53 +0100

    bookingapp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import List from './pages/list/List.js'
+import Home from './pages/home/Home.js'
+import Hotel from './pages/hotel/Hotel.js'
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/hotels' element={<List />} />
+        <Route path='/hotel/:id' element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './home.css'
+import Navbar from '../../components/navbar/Navbar.js'
+
+const Home = () => {
+  return (
+    <div> < Navbar/> </div>
+  )
+}
+
+export default Home;

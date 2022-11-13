7f8de8f34074fc04960a146b0a38c67dddf0628e
Z Ding <dinzonnyuoe@gmail.com>
2022-11-13 20:30:08 +0100

    a

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './home.css'
 import Navbar from '../../components/navbar/Navbar.js'
+import Header from '../../components/header/Header.js'
 
 const Home = () => {
   return (
-    <div> < Navbar/> </div>
+    <div>
+      < Navbar />
+      < Header />
+    </div>
   )
 }
 
--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar.js'
+import Header from '../../components/header/Header.js'
 
 const List = () => {
-  return (
-    <div> List</div>
+  return ( //in hotel page, we don't want to see the searchbox, so we will give a prop type to the header component. the prop can be taken in header.js and render the contents conditionally
+    <div>
+      <Navbar />
+      <Header type = "list" />
+    </div>
   )
 }
 
